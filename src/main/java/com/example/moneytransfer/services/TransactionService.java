a76fe9a55e5851cd639804788e2fb86e4f1b7892
Andrew Owino <andystudy4@gmail.com>
2023-03-12 02:00:04 +0300

    Annotate with @Transactional to ensure that the transfer operation is atomic

--- src/main/java/com/example/moneytransfer/services/TransactionService.java
+++ src/main/java/com/example/moneytransfer/services/TransactionService.java
@@ -1,8 +1,10 @@
 package com.example.moneytransfer.services;
 
 import com.example.moneytransfer.data.entities.Transaction;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 public interface TransactionService {
+    @Transactional
     Transaction transferMoney(Transaction transaction);
 }
