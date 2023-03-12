0ed2a8d9a56d83a3fb194fbedae725c4bf5e6b41
Andrew Owino <andystudy4@gmail.com>
2023-03-13 01:24:54 +0300

    Do away with email field for simplicity purposes

--- src/main/java/com/example/moneytransfer/data/entities/User.java
+++ src/main/java/com/example/moneytransfer/data/entities/User.java
@@ -16,7 +16,6 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
-    private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
@@ -40,29 +39,19 @@ public class User implements UserDetails {
         this.role = role;
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String password) {
         this.username = username;
-        this.email = email;
         this.password = password;
     }
 
-    public User(String username, String email, String password, Role role) {
+    public User(String username, String password, Role role) {
         this.username = username;
-        this.email = email;
         this.password = password;
         this.role = role;
     }
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public void setPassword(String password) {
