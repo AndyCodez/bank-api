37ba0ef88a4214d423696ca2e67db5b220ff6ac4
Andrew Owino <andystudy4@gmail.com>
2023-03-13 01:47:46 +0300

    Use consistent url

--- src/main/java/com/example/moneytransfer/controllers/ApiController.java
+++ src/main/java/com/example/moneytransfer/controllers/ApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1")
 public class ApiController {
 
     @Autowired
