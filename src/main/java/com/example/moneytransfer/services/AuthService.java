9a4a38fdc04280d8bb0413d710f321acc861e59d
Andrew Owino <andystudy4@gmail.com>
2023-03-13 01:34:17 +0300

    Throw an exception in case of wrong credentials Encrypt users' password upon registration

--- src/main/java/com/example/moneytransfer/services/AuthService.java
+++ src/main/java/com/example/moneytransfer/services/AuthService.java
@@ -3,10 +3,15 @@ package com.example.moneytransfer.services;
 import com.example.moneytransfer.data.entities.Role;
 import com.example.moneytransfer.data.entities.User;
 import com.example.moneytransfer.data.repositories.UserRepository;
+import com.example.moneytransfer.payloads.AuthRequest;
 import com.example.moneytransfer.payloads.AuthResponse;
 import com.example.moneytransfer.payloads.RegistrationRequest;
 import com.example.moneytransfer.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,19 +19,38 @@ public class AuthService {
 
     @Autowired
     private final UserRepository userRepository;
-
     @Autowired
     private final JwtUtil jwtUtil;
-    public AuthService(UserRepository userRepository, JwtUtil jwtUtil) {
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+    public AuthService(UserRepository userRepository, JwtUtil jwtUtil, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public AuthResponse registerUser(RegistrationRequest request) {
-        User user = new User(request.getUsername(), request.getEmail(), request.getPassword(), Role.USER);
+        User user = new User(request.getUsername(), passwordEncoder.encode(request.getPassword()), Role.USER);
+        return new AuthResponse(jwtUtil.generateToken(this.userRepository.save(user)));
+    }
 
-        var savedUser = this.userRepository.save(user);
+    public AuthResponse authenticateUser(AuthRequest request) throws Exception {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+        try {
+            authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect Username or Password", e);
+        }
 
-        return new AuthResponse(jwtUtil.generateToken(user));
+        var user =  this.userRepository.findByUsername(request.getUsername()).orElseThrow();
+
+        String jwtToken = jwtUtil.generateToken(user);
+
+        return new AuthResponse(jwtToken);
     }
 }
