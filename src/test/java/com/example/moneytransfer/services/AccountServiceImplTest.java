f0ae5e4eb74703cfc7c3b5e5070d5334cc3d2195
Andrew Owino <andystudy4@gmail.com>
2023-03-13 18:39:07 +0300

    Fix: Failed to load ApplicationContext

--- src/test/java/com/example/moneytransfer/services/AccountServiceImplTest.java
+++ src/test/java/com/example/moneytransfer/services/AccountServiceImplTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class AccountServiceImplTest {
 
     @Autowired
